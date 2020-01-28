ef9557c0631cb51b790c266964f98f74d485f0c7
JBinggi <juerg.binggeli@gmail.com>
2020-01-28 23:09:27 +0100

    added custom sql config example files isbn to isbn13 converter

--- src/Module.php
+++ src/Module.php
@@ -28,9 +28,9 @@ class Module {
     /**
      * Module Version
      *
-     * @since 1.0.6
+     * @since 1.0.0
      */
-    const VERSION = '1.0.6';
+    const VERSION = '1.0.1';
 
     /**
      * Load module config file
